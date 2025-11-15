9e0ea5d2619cca47775cc8225cb797febfcdca90
Rohan Khan <rk8399165+github@gmail.com>
2025-11-15 18:09:31 +0600

    use xformer swiglu

--- models/components.py
+++ models/components.py
@@ -2,18 +2,17 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from typing import Tuple, Optional
-
+from xformers.ops import SwiGLU
 
 class Expert(nn.Module):
     """Single expert network (essentially a FeedForward layer)"""
     def __init__(self, d_model: int, d_ff: int, dropout: float = 0.1):
         super().__init__()
-        self.linear1 = nn.Linear(d_model, d_ff, bias=False)
-        self.linear2 = nn.Linear(d_ff, d_model, bias=False)
+        self.ffn = SwiGLU(d_model, d_ff, d_model, bias=False)
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
-        return self.linear2(self.dropout(F.silu(self.linear1(x))))
+        return self.dropout(self.ffn(x))
 
 
 class TopKRouter(nn.Module):
